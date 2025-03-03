cecf6a303912bb242c6c5c077c0e06ed4c9ebffe
Takeru Ohta <phjgt308@gmail.com>
2025-03-03 19:47:59 +0900

    Update WriteJson trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
 pub trait WriteJson {
     type Error;
 
+    fn write_json_null(&mut self) -> Result<(), Self::Error>;
+    fn write_json_bool(&mut self, value: bool) -> Result<(), Self::Error>;
     fn write_json_i64(&mut self, value: i64) -> Result<(), Self::Error>;
+    fn write_json_f64(&mut self, value: f64) -> Result<(), Self::Error>;
     fn write_json_str(&mut self, value: &str) -> Result<(), Self::Error>;
+    fn write_json_array<F>(&mut self, f: F) -> Result<(), Self::Error>
+    where
+        F: FnOnce(&mut Self);
+    fn write_json_array_value<T: ToJson>(&mut self, value: &T) -> Result<(), Self::Error>;
+    fn write_json_object<F>(&mut self, f: F) -> Result<(), Self::Error>
+    where
+        F: FnOnce(&mut Self);
+    fn write_json_object_member<T: ToJson>(
+        &mut self,
+        name: &str,
+        value: &T,
+    ) -> Result<(), Self::Error>;
 }
 
 pub trait ReadJson {
