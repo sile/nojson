7e0107596ada73a16531dfbe44c875422cc95412
Takeru Ohta <phjgt308@gmail.com>
2025-03-26 08:02:02 +0900

    feat(value): extend Json trait with Display and FromStr, refactor JsonValue and Null type

--- src/value.rs
+++ src/value.rs
@@ -2,19 +2,20 @@ use std::{collections::BTreeMap, fmt::Display, str::FromStr};
 
 use crate::Error;
 
-pub trait Json {}
+pub trait Json: Display + FromStr {}
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum JsonValue {
-    Null(Null),
+    Null,
     Bool(bool),
-    Number(JsonNumber),
+    Integer(i64),
+    Float(f64),
     String(JsonString),
     Array(JsonArray),
     Object(JsonObject),
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Null;
 
 impl Json for Null {}
@@ -39,49 +40,26 @@ impl FromStr for Null {
 
 impl Json for bool {}
 
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-pub enum JsonNumber {
-    Integer(i64),
-    Float(f64),
-}
-
-impl Json for JsonNumber {}
-
-impl Display for JsonNumber {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            JsonNumber::Integer(v) => write!(f, "{v}"),
-            JsonNumber::Float(v) => write!(f, "{v}"),
-        }
-    }
-}
-
-impl FromStr for JsonNumber {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.contains('.') {
-            s.parse().map(Self::Float).map_err(Error::from)
-        } else {
-            s.parse().map(Self::Integer).map_err(Error::from)
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub struct JsonString<T = String>(pub T);
+pub struct JsonString<T = String>(T);
 
-impl<T> Json for JsonString<T> where T: AsRef<str> {}
+impl<T> JsonString<T> {
+    pub const fn new(s: T) -> Self {
+        Self(s)
+    }
+
+    pub fn get(&self) -> &T {
+        &self.0
+    }
+
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+// TODO: impl<T> Json for JsonString<T> where T: AsRef<str> {}
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct JsonArray<T = Vec<JsonValue>>(pub T);
-
-// impl<T, V> Json for JsonArray<T>
-// where
-//     T: AsRef<[V]>,
-//     V: Json,
-// {
-// }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct JsonObject<T = BTreeMap<String, JsonValue>>(pub T);
