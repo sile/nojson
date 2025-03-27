062457f9777c5d988943bf7fe475e0ebdde8ef15
Takeru Ohta <phjgt308@gmail.com>
2025-03-27 09:30:52 +0900

    Refactor Value enum and add Error, PathItem structures for improved JSON handling

--- src/value2.rs
+++ src/value2.rs
@@ -1,33 +1,49 @@
-use std::str::FromStr;
+use std::{borrow::Cow, str::FromStr};
+
+use crate::value::Json;
 
 #[derive(Debug)]
+pub struct Error {
+    pub path: Vec<PathItem>,
+    pub reason: ErrorReason,
+    pub cause: Option<Box<dyn 'static + std::error::Error>>,
+}
+
+#[derive(Debug)]
+pub enum ErrorReason {}
+
+#[derive(Debug)]
+pub enum PathItem {
+    Index(usize),
+    Name(String),
+}
+
+#[derive(Debug, Clone)]
 pub enum Value<'a> {
-    Null(Null<'a>),
-    Bool(Bool<'a>),
+    Null,
+    Bool(bool),
+    Number(JsonNumber<'a>),
+    String(JsonString<'a>),
 }
 
 impl<'a> Value<'a> {
-    pub fn parse<T: FromStr>(&self) -> Result<T, T::Err> {
-        match self {
-            Value::Null(v) => v.text.parse(),
-            Value::Bool(v) => v.text.parse(),
-        }
+    pub fn parse<T>(&self) -> Result<T, T::Err>
+    where
+        T: Json + FromStr,
+        Error: From<T::Err>,
+    {
+        todo!()
     }
+
+    // parse_nullable
 }
 
-#[derive(Debug)]
-pub struct Null<'a> {
-    pub text: &'a str,
+#[derive(Debug, Clone)]
+pub struct JsonString<'a> {
+    pub text: Cow<'a, str>,
 }
 
-impl Null<'static> {
-    pub const fn new() -> Self {
-        Self { text: "null" }
-    }
-}
-
-#[derive(Debug)]
-pub struct Bool<'a> {
-    pub value: bool,
-    pub text: &'a str,
+#[derive(Debug, Clone)]
+pub struct JsonNumber<'a> {
+    pub text: Cow<'a, str>,
 }
