8012f450146da7f366fbfe885a1228fc35a939f1
Takeru Ohta <phjgt308@gmail.com>
2025-03-29 10:14:29 +0900

    Refactor JsonStr to use Cow for text and values and update lifetime annotations

--- src/str.rs
+++ src/str.rs
@@ -1,4 +1,4 @@
-use std::{hash::Hash, num::NonZeroUsize, ops::Range, str::FromStr};
+use std::{borrow::Cow, hash::Hash, num::NonZeroUsize, ops::Range, str::FromStr};
 
 // TODO: private
 pub const WHITESPACES: [char; 4] = [' ', '\t', '\r', '\n'];
@@ -30,41 +30,49 @@ struct JsonValue {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct JsonStr<'t> {
-    text: &'t str,
+pub struct JsonStr<'text, 'index> {
+    text: Cow<'text, str>,
     index: usize,
-    values: Vec<JsonValue>,
+    values: Cow<'index, [JsonValue]>,
 }
 
-impl<'t> PartialOrd for JsonStr<'t> {
+impl<'text, 'index> PartialOrd for JsonStr<'text, 'index> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<'t> Ord for JsonStr<'t> {
+impl<'text, 'index> Ord for JsonStr<'text, 'index> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        (self.text, self.index).cmp(&(other.text, other.index))
+        (&self.text, self.index).cmp(&(&other.text, other.index))
     }
 }
 
-impl<'t> Hash for JsonStr<'t> {
+impl<'text, 'index> Hash for JsonStr<'text, 'index> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        (self.text, self.index).hash(state);
+        (&self.text, self.index).hash(state);
     }
 }
 
-impl<'t> JsonStr<'t> {
-    pub fn new(text: &'t str) -> Result<Self, Error> {
+impl<'text> JsonStr<'text, 'static> {
+    pub fn new(text: &'text str) -> Result<Self, Error> {
         Ok(Self {
-            text,
+            text: Cow::Borrowed(text),
             index: 0,
-            values: Vec::new(),
+            values: Cow::Owned(Vec::new()),
         })
     }
+}
 
-    pub fn text(&self) -> &'t str {
-        self.text
+impl<'text, 'index> JsonStr<'text, 'index> {
+    pub fn text(&self) -> &str {
+        &self.text
+    }
+
+    // TODO: remainin_text()
+
+    pub fn kind(&self) -> JsonStrKind {
+        self.values[self.index].kind
     }
 
     pub fn nullable<F, T>(&self, _f: F) -> Result<Option<T>, Error>
@@ -90,35 +98,35 @@ impl<'t> JsonStr<'t> {
         todo!()
     }
 
-    pub fn expect_array(&self) -> Result<JsonArrayStr<'_, 't>, Error> {
-        todo!()
-    }
+    // pub fn expect_array(&self) -> Result<JsonArrayStr<'_, 'text>, Error> {
+    //     todo!()
+    // }
 
     // TODO: kind()
     // TODO: parse_null(), parse_bool(), parse_float(), parse_number(), parse_string()
     // TODO: array(), object()
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct JsonArrayStr<'a, 't> {
-    s: &'a JsonStr<'t>,
-}
+//#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+// pub struct JsonArrayStr<'a, 't> {
+//     s: &'a JsonStr<'t>,
+// }
 
-impl<'a, 't> JsonArrayStr<'a, 't> {
-    pub fn get(&self, _index: usize) -> Result<&'a JsonStr<'t>, Error> {
-        todo!()
-    }
-}
+// impl<'a, 't> JsonArrayStr<'a, 't> {
+//     pub fn get(&self, _index: usize) -> Result<&'a JsonStr<'t>, Error> {
+//         todo!()
+//     }
+// }
 
-impl<'a, 't> Iterator for JsonArrayStr<'a, 't> {
-    type Item = &'a JsonStr<'t>;
+// impl<'a, 't> Iterator for JsonArrayStr<'a, 't> {
+//     type Item = &'a JsonStr<'t>;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!()
-    }
-}
+//     fn next(&mut self) -> Option<Self::Item> {
+//         todo!()
+//     }
+// }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct JsonObjectStr<'a> {
-    json: &'a str,
-}
+// #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+// pub struct JsonObjectStr<'a> {
+//     json: &'a str,
+// }
