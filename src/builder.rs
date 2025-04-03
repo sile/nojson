cd7e618d71ab2bc648ad5c1ab4596b80e7e67976
Takeru Ohta <phjgt308@gmail.com>
2025-04-03 10:30:00 +0900

    feat: add JsonValue and JsonArray structures for JSON building

--- src/builder.rs
+++ src/builder.rs
@@ -2,6 +2,48 @@ use std::fmt::Display;
 
 use crate::DisplayJson;
 
+// pub fn value_with<F>(f:F) ->
+// pub fn array_with()
+// pub fn object_with()
+// Json(json_with(|f| ...)).to_string()
+
+pub struct JsonValue;
+
+impl JsonValue {
+    // fn null(self) -> impl DisplayJson {
+    //     todo!()
+    // }
+
+    // pub fn maybe_finite_f64()
+    // pub fn string()
+
+    // pub fn array<F>(self, f: F) -> impl DisplayJson
+    // where
+    //     F: FnOnce(&mut JsonArray),
+    // {
+    //     todo!()
+    // }
+}
+
+pub struct JsonArray;
+
+impl JsonArray {
+    pub fn value<T>(&mut self, _value: T)
+    where
+        T: DisplayJson,
+    {
+        // Json(JsonValue::Array(|a| {
+        //     a.value("foo");
+        //     a.value(1);
+        //     a.values(iter)
+        // }))
+        // .to_string();
+        todo!()
+    }
+
+    // values
+}
+
 pub struct JsonFormatter<'a> {
     fmt: &'a mut std::fmt::Formatter<'a>,
 }
@@ -48,4 +90,26 @@ impl<'a> JsonFormatter<'a> {
     {
         todo!()
     }
+
+    // pub fn array(self) -> JsonArrayFormatter<'a> {
+    //     let ok = write!(self.fmt, "[").is_ok();
+    //     JsonArrayFormatter {
+    //         inner: ok.then_some(self),
+    //         first: true,
+    //     }
+    // }
 }
+
+// pub struct JsonArrayFormatter<'a> {
+//     inner: Option<JsonFormatter<'a>>,
+//     first: bool,
+// }
+
+// impl<'a> JsonArrayFormatter<'a> {
+//     pub fn value<T>(&mut self, _v: T) -> std::fmt::Result
+//     where
+//         T: DisplayJson,
+//     {
+//         todo!()
+//     }
+// }
