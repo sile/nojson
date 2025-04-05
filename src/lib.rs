8be1109162e104332f11ff594192c93c067a5b04
Takeru Ohta <phjgt308@gmail.com>
2025-04-06 08:26:55 +0900

    chore: add doc comments and missing_docs warning

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-//!
+//! TODO
 //! - One-to-one mapping between Rust types and JSON texts is not needed.
 //!   - You can gain the merits of both type-level probgramming and flexibility of imperative code.
 //! - Rather toolbox than a monilitic framework.
 //! - Easy to add custom validtions:
 //!   - Application specific validation error can be associated with the errorneous JSON value position at the JSON text.
+#![warn(missing_docs)]
+
 mod display_json;
 mod format;
 mod from_raw_json_value;
@@ -20,8 +22,41 @@ pub use from_raw_json_value::FromRawJsonValue;
 pub use kind::JsonValueKind;
 pub use raw::{JsonParseError, RawJson, RawJsonValue};
 
+/// Marker struct that makes it possible to execute JSON parsing and generation via [`FromStr`] and [`Display`] traits.
+///
+/// This is a handy way to operating on JSON, but if you need more control,
+/// please consider to use [`RawJson`] (for JSON pasing) and [`json()`] (for JSON generation) instead.
+///
+/// # Examples
+///
+/// Parse a JSON text:
+/// ```
+/// use nojson::Json;
+///
+/// # fn main() -> Result<(), nojson::JsonParseError> {
+/// // As `[Option<u32>; 3]` type implements the `FromRawJsonValue` trait,
+/// // you can use the `std::str::parse()` method to parse JSON by wrapping the type by `Json`.
+/// let text = "[1, null, 2]";
+/// let value: Json(_) = text.parse()?;
+/// assert_eq!(value, [Some(1), None, Some(2)]);
+/// # OK(())
+/// # }
+/// ```
+///
+/// Generate a JSON type from a Rust type:
+/// ```
+/// use nojson::Json;
+///
+/// # fn main() -> Result<(), nojson::JsonParseError> {
+/// // As `[Option<u32>; 3]` type implements the `DisplyJson` trait too,
+/// // you can use the `std::fmt::Display::to_string()` method to generate JSON by wrapping the type by `Json`.
+/// let value = [Some(1), None, Some(2)];
+/// assert_eq!(Json(value).to_string(), "[1,null,2]");
+/// # OK(())
+/// # }
+/// ```
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Json<T>(pub T);
+pub struct Json<T>(#[allow(missing_docs)] pub T);
 
 impl<T: DisplayJson> Display for Json<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
