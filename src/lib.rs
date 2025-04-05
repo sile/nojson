08e5f3aa202262c7c8ea323b5277dee02dc22601
Takeru Ohta <phjgt308@gmail.com>
2025-04-05 19:05:47 +0900

    Move `json` function and `InplaceJson` implementation from format.rs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ mod raw;
 use std::{fmt::Display, str::FromStr};
 
 pub use display_json::DisplayJson;
-pub use format::{JsonArrayFormatter, JsonFormatter, JsonObjectFormatter, json};
+pub use format::{JsonArrayFormatter, JsonFormatter, JsonObjectFormatter};
 pub use from_raw_json_value::FromRawJsonValue;
 pub use kind::JsonValueKind;
 pub use raw::{JsonParseError, RawJson, RawJsonValue};
@@ -40,5 +40,32 @@ where
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let raw = RawJson::parse(s)?;
         raw.value().try_to().map(Self)
+    }
+}
+
+pub fn json<F>(f: F) -> impl DisplayJson + Display
+where
+    F: Fn(&mut JsonFormatter<'_, '_>) -> std::fmt::Result,
+{
+    InplaceJson(f)
+}
+
+struct InplaceJson<F>(F);
+
+impl<F> Display for InplaceJson<F>
+where
+    F: Fn(&mut JsonFormatter<'_, '_>) -> std::fmt::Result,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Json(self))
+    }
+}
+
+impl<F> DisplayJson for InplaceJson<F>
+where
+    F: Fn(&mut JsonFormatter<'_, '_>) -> std::fmt::Result,
+{
+    fn fmt(&self, f: &mut JsonFormatter<'_, '_>) -> std::fmt::Result {
+        self.0(f)
     }
 }
